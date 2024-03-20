91701ec2007962010b29e876d33ae807eff11413
Andrew Berry <andrew.berry@hearst.com>
2024-03-20 16:03:24 -0400

    Updating for gh-pages

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  transpileDependencies: true
-  publicPath: process.env.NODE_ENV === "production" ? "/vault-tool/" : "/",
-})
+  transpileDependencies: true,
+  publicPath: "/vault-tool/",
+});
